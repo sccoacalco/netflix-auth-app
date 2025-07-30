f0e7b8ed7882b926e9cba829d322a0a5f8676344
Juan Coacalco <sc.coacalco@cecytem.mx>
2025-07-30 13:49:43 -0600

    segundo cambio

--- users/views.py
+++ users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+# users/views.py
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+User = get_user_model()
+
+class RegisterView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        if User.objects.filter(username=username).exists():
+            return Response({'error': 'El usuario ya existe'}, status=status.HTTP_400_BAD_REQUEST)
+
+        user = User.objects.create_user(username=username, password=password)
+        return Response({'message': 'Usuario creado con éxito'}, status=status.HTTP_201_CREATED)
